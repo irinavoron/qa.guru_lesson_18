1b033ddfaa8d9f1283908e0e54cee2a3a965deef
Irina.Voron <yarutich.irina@gmail.com>
2024-04-05 18:16:26 +0300

    Add pydantic_settings

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,25 @@
 import pytest
 from selene import browser
+import pydantic_settings
+
+
+class Config(pydantic_settings.BaseSettings):
+    base_url: str = 'https://demowebshop.tricentis.com'
+    window_width: int = 1896
+    window_height: int = 1080
+    timeout: float = 4.5
+
+
+config = Config()
 
 
 @pytest.fixture(autouse=True, scope='function')
 def browser_management():
-    browser.config.base_url = 'https://demowebshop.tricentis.com'
-    browser.config.window_width = 1896
-    browser.config.window_height = 1080
+    browser.config.base_url = config.base_url
+    browser.config.window_width = config.window_width
+    browser.config.window_height = config.window_height
+    browser.config.timeout = config.timeout
+
+    yield
+
+    browser.quit()
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,11 +1,15 @@
-from selene import browser, command, have
+import allure
+from selene import browser, have
 
 
 def test_login():
-    browser.open('/login')
+    with allure.step('Open login page'):
+        browser.open('/login')
 
-    browser.element('#Email').type('j.doe@example.mail.com')
-    browser.element('#Password').type('123456')
-    browser.element('.login-button').click()
+    with allure.step('Fill login data'):
+        browser.element('#Email').type('j.doe@example.mail.com')
+        browser.element('#Password').type('123456')
+        browser.element('.login-button').click()
 
-    browser.element('.account').should(have.text('j.doe@example.mail.com'))
+    with allure.step('Account data should be displayed'):
+        browser.element('.account').should(have.text('j.doe@example.mail.com'))
