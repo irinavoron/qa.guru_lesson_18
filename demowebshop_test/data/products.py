f73ad08f7fed0c36300261f0bbbd7e8638999ba3
Irina.Voron <yarutich.irina@gmail.com>
2024-04-22 20:41:59 +0300

    Add products.py

--- demowebshop_test/data/products.py
+++ demowebshop_test/data/products.py
@@ -3,3 +3,13 @@ from dataclasses import dataclass
 
 @dataclass
 class Product:
+    name: str
+    price: float
+    add_to_cart_endpoint: str
+
+
+notebook = Product(
+    name='14.1-inch Laptop',
+    price=1590.00,
+    add_to_cart_endpoint='/addproducttocart/details/31/1'
+)
--- tests/test_demowebshop.py
+++ tests/test_demowebshop.py
@@ -1,23 +1,26 @@
 import allure
 from selene import browser, have
 
+from demowebshop_test.data import products
 from demowebshop_test.moduls import api_methods
 from demowebshop_test.moduls.cart import Cart
 
 cart = Cart()
+
+cookie_name = 'NOPCOMMERCE.AUTH'
 
 
 def test_product_added_to_cart():
     authorization_cookie = api_methods.get_authorization_cookie()
     api_methods.login(authorization_cookie)
 
-    api_methods.add_product_to_cart(product_endpoint='/addproducttocart/catalog/31/1/1',
-                                    cookies={'NOPCOMMERCE.AUTH': authorization_cookie}
+    api_methods.add_product_to_cart(product_endpoint=products.notebook.add_to_cart_endpoint,
+                                    cookies={cookie_name: authorization_cookie}
                                     )
     cart.open_cart()
 
     with allure.step('Only the added product should be displayed in the cart'):
-        browser.element('.product-name').should(have.exact_text('14.1-inch Laptop'))
+        browser.element('.product-name').should(have.exact_text(products.notebook.name))
         browser.element('[name^=itemquantity]').should(
             have.attribute('value', '1')
         )
@@ -30,8 +33,8 @@ def test_added_product_can_be_deleted():
     authorization_cookie = api_methods.get_authorization_cookie()
     api_methods.login(authorization_cookie)
 
-    api_methods.add_product_to_cart(product_endpoint='/addproducttocart/catalog/31/1/1',
-                                    cookies={'NOPCOMMERCE.AUTH': authorization_cookie}
+    api_methods.add_product_to_cart(product_endpoint=products.notebook.add_to_cart_endpoint,
+                                    cookies={cookie_name: authorization_cookie}
                                     )
     cart.open_cart()
     cart.remove_product_from_the_cart()
